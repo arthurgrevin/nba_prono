f9a664a3f97e116e6cd7e8ad59c5b1ae202d51d8
ARTHUR GREVIN <arthur.grevin@gmail.com>
2017-01-25 00:59:23 +0100

    set travis

--- server/src/tests/MatchDAOTest.ts
+++ server/src/tests/MatchDAOTest.ts
@@ -2,31 +2,55 @@ process.env.NODE_ENV = 'test';
 import 'reflect-metadata';
 
 import { MatchDAO } from "../dao/MatchDAO";
+import { TeamDAO } from "../dao/TeamDAO";
 import { Match } from "../entities/Match";
 import { Prono } from "../entities/Prono";
+import { Team } from "../entities/Team";
+import { Player } from "../entities/Player";
 import { connection } from "../dao/database";
 import * as chai from 'chai'
 describe('MatchDAO', () => {
-    let matchDAO: MatchDAO = new MatchDAO(connection);
+    const matchDAO: MatchDAO = new MatchDAO(connection);
+    const teamDAO: TeamDAO = new TeamDAO(connection);
     beforeEach(() => {
-        matchDAO = new MatchDAO(connection);
     });
 
     before("#findMatches", (done) => {
+        const player: Player = {
+            username: "Arthur",
+            password: "test",
+
+        }
+        const team1: Team = {
+            key: "GS",
+            city: "Golden State",
+            name: "warriors",
+            logo: "/"
+        }
+        const team2: Team = {
+            key: "HOU",
+            city: "Houston",
+            name: "rockets",
+            logo: "/"
+        }
+        const promises = Promise.all([teamDAO.saveTeam(team1), teamDAO.saveTeam(team2)]);
+
         const date: number = Date.parse("2005-07-08T06:00:00+0200");
         let matchTest: Match = new Match();
         matchTest.date = date;
-        /*matchTest.homeKey = 'GS';
-        matchTest.awayKey = 'OKC';
-*/
-        const prono: Prono = new Prono();
-        prono.choice = 'GS'
+        matchTest.home = team1;
+        matchTest.away = team2;
 
-        matchDAO.saveMatch(matchTest)
+        let prono: Prono = new Prono();
+        prono.choice = 'GS';
+        prono.player = player;
+        prono.match = matchTest;
+        console.log(matchTest)
+        promises.then(() => matchDAO.saveMatch(matchTest)
             .then(response => {
                 done()
             })
-            .catch(err => console.log('err'))
+            .catch(err => console.log(err)))
     })
 
     describe('#findMatches', () => {
@@ -36,8 +60,8 @@ describe('MatchDAO', () => {
             matchDAO.findMatches()
                 .then(response => {
                     console.log(response)
-                    /*chai.assert(response[0].homeKey == 'GS')
-                    chai.assert(response[0].awayKey == 'OKC')*/
+                    chai.assert(response[0].home.key == 'GS')
+                    chai.assert(response[0].away.key == 'HOU')
                     chai.assert.deepEqual(response[0].date, date, date.toString())
                     done()
                 })
