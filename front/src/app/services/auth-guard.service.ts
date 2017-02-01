dcf1ac353e9d9b0fb0938d5cef2ebcc769738b4d
ARTHUR GREVIN <arthur.grevin@gmail.com>
2017-02-01 22:24:36 +0100

    reload when token expired

--- front/src/app/services/auth-guard.service.ts
+++ front/src/app/services/auth-guard.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 
 @Injectable()
-export class AuthGuardService implements CanActivate{
+export class AuthGuardService implements CanActivate {
 
-  constructor(private router:Router) { }
+  constructor(private router: Router) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     console.log(localStorage.getItem('currentPlayer'))
-    if(localStorage.getItem('currentPlayer')){
+    if (localStorage.getItem('currentPlayer')) {
       return true;
     }
-    this.router.navigate(['/login'],{ queryParams: { returnUrl: state.url }})
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } })
     return false;
   }
 }
--- front/src/app/services/match.service.ts
+++ front/src/app/services/match.service.ts
@@ -1,49 +1,50 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import 'rxjs/add/operator/toPromise';
 import { Observable } from 'rxjs/Observable';
-import {Match} from "../entities/match";
+import { Match } from "../entities/match";
+
 
 @Injectable()
 export class MatchService {
 
 
-  private url :string="/api/v1/matchs";
-  constructor(private http:Http) { }
+  private url: string = "/api/v1/matchs";
+  constructor(private http: Http) { }
 
-    getMatchs():Promise<Match[]>{
-      return this.http
-      .get(this.url,this.jwt())
+  getMatchs(): Promise<Match[]> {
+    return this.http
+      .get(this.url, this.jwt())
       .toPromise()
       .then(response => response.json() as Match[])
   }
-  
-  getMatchByDate(date:Date):Observable<any>{
+
+  getMatchByDate(date: Date): Observable<any> {
     const dateString = date.toISOString()
     const playerId = JSON.parse(localStorage.getItem('currentPlayer')).id
-    const url = this.url + "/" + dateString + "?playerId="+playerId
+    const url = this.url + "/" + dateString + "?playerId=" + playerId
     console.log(this.jwt())
-    return this.http.get(url,this.jwt())
-        .map(response=>{
-          return response.json() as Match[]
-        })
-        .catch((e)=>{
-          this.handleError(e)
-          return null;
-        });
+    return this.http.get(url, this.jwt())
+      .map(response => {
+        return response.json() as Match[]
+      })
+      .catch((e) => {
+        this.handleError(e)
+        return null;
+      });
   }
 
-  private handleError(e){
-    if(e.status==401){
+  private handleError(e) {
+    if (e.status == 401) {
       localStorage.removeItem('currentPlayer');
+      window.location.reload(true);
     }
   }
 
-  private jwt(){
-    let currentPlayer= JSON.parse(localStorage.getItem('currentPlayer'));
+  private jwt() {
+    let currentPlayer = JSON.parse(localStorage.getItem('currentPlayer'));
     console.log(JSON.parse(localStorage.getItem('currentPlayer')));
     console.log(currentPlayer);
-    if(currentPlayer){
+    if (currentPlayer) {
       const headers = new Headers({ 'x-access-token': currentPlayer.jwt });
       return new RequestOptions({ headers: headers });
     }
